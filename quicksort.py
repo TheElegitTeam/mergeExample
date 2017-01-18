163eab403f17ae4177dada9a3fd9ac529bd759a0
jonesh2 <jonesh2@carleton.edu>
2017-01-17 22:06:17 -0600

    removed somethings from quicksort helper, added in code for partition function. changed list in main function

--- quicksort.py
+++ quicksort.py
@@ -1,15 +1,35 @@
-def quickSort(aList)
-    quickSortHelper(aList, 0, len(list)-1)
+def quickSort(theList)
+    quickSortHelper(aList, 0, len(theList)
 
-def quickSortHelper(aList, start, end)
-    if start < end:                            # If there are two or more elements...
-        split = partition(aList, start, end)    # ... partition the sublist...
-        quicksort(aList, start, split-1)        # ... and sort both halves.
-        quicksort(aList, split+1, end)
+def quickSortHelper(theList, start, end):
+    if start <= end:                            # If there are two or more elements...
+        split = partition(theList, start, end)    # ... partition the sublist...
     else:
         return
 
+def partition(theList, start, end):
+    pivot = theList[end]                       # Partition around the last value
+    bottom = start-1                           # Start outside the area to be partitioned
+    top = end                                  # Ditto
+
+    done = 0
+    while not done:                            # Until we find an out of place element...
+        top = top-1                            # ... move the top down.
+
+        if top == bottom:                      # If we hit the bottom...
+            done = 1                           # ... we are done.
+            break
+
+        if theList[top] < pivot:               # Is the top out of place?
+            theList[bottom] = theList[top]     # Then put it at the bottom...
+            break                              # ...and start searching from the bottom.
+
+
+    theList[top] = pivot                       # Put the pivot in its place.
+    return top                                 # Return the split point
+
+
 def main():
-    myList = [3, 2, 5, 1, 9, 0, 0, 4, 45, 12]
+    myList = [3, 2, 33, 1, 9, 90, 0, 5, 31, 12]
     quickSort(myList)
     print(myList)
