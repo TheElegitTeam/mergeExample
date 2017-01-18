ef06cf381c99ae3133eb53700046c0fe5b889630
jonesh2 <jonesh2@carleton.edu>
2017-01-17 21:55:42 -0600

    changed variable name. fixed main to int the input and call main.

--- caesar.py
+++ caesar.py
@@ -3,7 +3,7 @@ def caesar(plaintext, offset):
     in the plaintext by a Caesar cipher by the specified shift.
     Non-letter characters should remain unchanged in the ciphertext.'''
 
-    ciphertext = ""
+    spywords = ""
 
     for ch in plaintext:
         # to shift uppercase letters
@@ -24,15 +24,15 @@ def caesar(plaintext, offset):
                 print ch
 
         # adds shifted character to empty string
-        ciphertext += ch
+        spywords += ch
 
-    return ciphertext
+    return spywords
 
 def main():
     message = raw_input('Enter a message: ')
-    shift = raw_input('Enter a non-negative integer: ')
+    shift = int(raw_input('Enter a non-negative integer: '))
 
     ciphertext = caesar(message, shift)
     print ciphertext
 
-print ciphertext
+main()
